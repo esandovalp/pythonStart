27f6fa8576c4816d07d7fad96f963fcae3a02f91
esandovalp <100039561+esandovalp@users.noreply.github.com>
2022-11-03 21:41:15 -0600

    print a matrix final

---
+++ print_matrix.py
@@ -0,0 +1,4 @@
+identity_matrix = [[1,0,0],[0,1,0],[0,0,1]]
+
+for i in identity_matrix:
+   print(i, flush = " ")
--- test.py
+++
@@ -1,8 +0,0 @@
-from array import *
-
-identityMatrix = [[1,0,0],[0,1,0],[0,0,1]]
-
-for i in identityMatrix:
-   for j in i:
-      print(i, end = " ")
-   print()
